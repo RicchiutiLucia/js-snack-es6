28e1e55ae7c3017032590d55eccc9e78099b6795
RicchiutiLucia <luciaricchiuti04@gmail.com>
2023-03-10 14:31:50 +0100

    snack 3 concluso

--- snack_3/js/main.js
+++ snack_3/js/main.js
@@ -3,28 +3,29 @@ La funzione ritornerà un nuovo array con i valori che
 hanno la posizione compresa tra i due numeri.
 Usiamo i nuovi metodi degli array foreach o filter.*/
 
-//funzione con filter
+
 const myArray = [4,8,24,28,7,33,37,44,21,36,49,50,2];
 let newArray = filterArray(myArray, 4,7);
 console.log(myArray);
 console.log(newArray);
 
-
+//funzione con filter
 function filterArray(array, a, b){
   return array.filter((i) => {
       return a <= i && b >= i});
 }
-//funzione con forEach
+
 const forEachArray = [3,8,24,22,10,34,37,44,21,39,51,2];
 let arrayTarget = arrayForEach(forEachArray,2,8);
 console.log(arrayTarget);
 
+
+//funzione con forEach
 function arrayForEach(array,a,b){
 
   const newArray2 = [];
 
   array.forEach((value, i) => {
-      console.log(value);
       if(a < i && b > i){
           newArray2.push(value);
       }
